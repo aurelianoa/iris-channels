20f85c9a9509b4dffc2337fbd8c8b704222637a7
Aureliano Alarcon Smith <aureliano.alarcon@me.com>
2023-06-30 09:57:41 -0400

    Proper naming convention

--- contracts/channel/IChannel.sol
+++ contracts/channel/IChannel.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.17;
 
 interface IChannel {
-    function broadcast(string memory eventIndex, bytes memory data) external;
-    function setChannel(address emitter, address[] memory listeners) external;
-    function setChannelStatus(address emitter, bool active) external;
-    function setChannelLazyStatus(address emitter, bool lazy) external;
-    function setListener(address emitter, address listener) external;
-    function removeChannelListener(address emitter, address listener) external;
+    function broadcast(address _channel, string memory eventIndex, bytes memory data) external;
+    function supportsIChannelInterface(bytes4 interfaceId) external view returns (bool);
 }
--- contracts/emitter/IEmitter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IEmitter {
-    function broadcast(address _channel, string memory eventIndex, bytes memory data) external;
-    function supportsIEmitterInterface(bytes4 interfaceId) external view returns (bool);
-}
