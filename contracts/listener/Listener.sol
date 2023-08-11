1d2ad147b0e9a190811d97fc471d38c608e7e759
Aureliano Alarcon Smith <aureliano.alarcon@me.com>
2023-08-11 10:34:04 -0400

    validating channel sender

--- contracts/listener/Listener.sol
+++ contracts/listener/Listener.sol
@@ -12,6 +12,10 @@ abstract contract Listener is IListener {
     /// @dev channel => Message
     mapping(address => Message) internal lastMessages;
 
+    /// mapping of channels
+    /// @dev channel => bool
+    mapping(address => bool) internal channels;
+
     /// @inheritdoc IListener
     function listen(address channel, string memory eventIndex, bytes memory data) virtual public {
         Message memory _message = Message(eventIndex, data);
@@ -35,6 +39,28 @@ abstract contract Listener is IListener {
     /// @inheritdoc IListener
     function supportsIListenerInterface(bytes4 interfaceId) public view virtual returns (bool) {
         return interfaceId == type(IListener).interfaceId;
+    }
+
+    /// set available channel
+    /// @param channel address
+    /// @param active bool
+    function setChannel(address channel, bool active) public virtual {
+        channels[channel] = active;
+    }
+
+    /// check if channel is available
+    /// @param channel address
+    /// @return bool
+    function isChannelAvailable(address channel) public view virtual returns (bool) {
+        return channels[channel];
+    }
+
+    /// check if message is legit
+    /// @param channel address
+    /// @param eventIndex string
+    /// @return bool
+    function isLegitMessage(address channel, string memory eventIndex) public view virtual returns (bool) {
+        return isChannelAvailable(channel) && !compareIndex(lastMessages[channel].eventIndex, eventIndex);
     }
 }
 
